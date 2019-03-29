59ad8450db3141f1502bb61a7f5765bfebf9fb09
marioskan <marios1997@yahoo.gr>
2019-03-29 15:52:34 +0200

    Azure Cognitive
    TextToSpeech Console Application for StudentGuru Cognitive Workshop

---
+++ TextToSpeech/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+
+namespace helloworld
+{
+    class Program
+    {
+        public static async Task RecognizeSpeechAsync()
+        {
+            
+            var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourRegion");
+
+            
+            using (var recognizer = new SpeechRecognizer(config))
+            {
+                Console.WriteLine("Say something...");
+
+               
+                var result = await recognizer.RecognizeOnceAsync();
+
+               
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine($"We recognized: {result.Text}");
+                }
+                else if (result.Reason == ResultReason.NoMatch)
+                {
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                }
+            }
+        }
+
+        static int Main()
+        {
+            RecognizeSpeechAsync().Wait();
+            Console.WriteLine("Please press a key to continue.");
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
